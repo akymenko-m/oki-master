c625b634e9c323f6bb41eeb0b386d06a68feff12
Mariia Akymenko <dafnarianna@gmail.com>
2023-08-17 00:10:33 +0300

    initial project

---
+++ .eslintrc.cjs
@@ -0,0 +1,30 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    'airbnb',
+    'airbnb-typescript',
+    'airbnb/hooks',
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:react/recommended',
+    'prettier',
+  ],
+  overrides: [],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+    project: ['./tsconfig.json', './tsconfig.node.json'],
+    // project: './tsconfig.json',
+  },
+  plugins: ['react', '@typescript-eslint'],
+  rules: {
+    'react/react-in-jsx-scope': 0,
+    'import/no-extraneous-dependencies': ['error', { devDependencies: true }],
+  },
+
+  // ignorePatterns: ['.eslintrc.cjs', 'vite.config.ts', 'src'],
+};
