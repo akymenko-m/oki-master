ec7aa6f6d60fdee5cbbef1b4c391cc5be6bb64dc
Mariia Akymenko <dafnarianna@gmail.com>
2023-08-17 22:04:24 +0300

    add styles to main app

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
+import { Global, ThemeProvider } from '@emotion/react';
+import GlobalStyles from './styles/GlobalStyles';
 import App from './App';
+import theme from './styles/theme';
+// import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider theme={theme}>
+      <Global styles={GlobalStyles} />
+      <App />
+    </ThemeProvider>
   </React.StrictMode>
 );
