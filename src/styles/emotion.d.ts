b9b6aeac164736803c7c0b896bf084c176497ccd
Mariia Akymenko <dafnarianna@gmail.com>
2023-08-22 23:24:36 +0300

    + added schema interface

---
+++ src/styles/emotion.d.ts
@@ -0,0 +1,64 @@
+import '@emotion/react';
+
+declare module '@emotion/react' {
+  export interface Theme {
+    colors: {
+      text: string;
+      secondary: string;
+      white: string;
+      accent: string;
+      dark: string;
+      light: string;
+      'light-20': string;
+      mainBackground: string[];
+      aboutUsBackground: string[];
+      iconBackground: string[];
+      iconActive: string[];
+    };
+    fontSizes: {
+      desktop: {
+        xs: string;
+        small: string;
+        medium: string;
+        large: string;
+        xl: string;
+        xxl: string;
+        xxxl: string;
+      };
+      mobile: {
+        xs: string;
+        small: string;
+        medium: string;
+        large: string;
+        xl: string;
+      };
+    };
+    letterSpasing: {
+      none: string;
+      auto: string;
+      small: string;
+      medium: string;
+      large: string;
+    };
+    breakpoints: {
+      s: string;
+      m: string;
+      l: string;
+    };
+    lineHeight: {
+      auto: string;
+      small: string;
+      medium: string;
+      large: string;
+      xl: string;
+      xxl: string;
+    };
+    animations: {
+      cubicBezier: string;
+    };
+    spacing: {
+      step: number;
+    };
+    duration: string;
+  }
+}
