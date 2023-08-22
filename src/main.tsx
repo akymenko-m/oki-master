d5c987115c4eeb855790a69cd259df909c65af53
Mariia Akymenko <dafnarianna@gmail.com>
2023-08-22 23:31:08 +0300

    + updated App path

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { Global, ThemeProvider } from '@emotion/react';
 import GlobalStyles from './styles/GlobalStyles';
-import App from './App';
+import App from './components/App/App';
 import theme from './styles/theme';
 // import './index.css';
 
