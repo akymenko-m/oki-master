9971104035fac051fd81fded4cee4b4e759461e2
Mariia Akymenko <dafnarianna@gmail.com>
2023-08-22 23:30:31 +0300

    + updated App component add Header component

---
+++ src/components/App/App.tsx
@@ -0,0 +1,16 @@
+// import { useState } from 'react';
+// import reactLogo from './assets/react.svg';
+// import viteLogo from '../../../../../vite.svg';
+// import viteLogo from '../public/vite.svg';
+// import './App.css';
+import Header from '../Header/Header';
+
+function App() {
+  return (
+    <div>
+      <Header />
+    </div>
+  );
+}
+
+export default App;
