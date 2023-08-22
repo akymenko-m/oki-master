476c6116971520323cc15f7a3f6911a24017aaf8
Mariia Akymenko <dafnarianna@gmail.com>
2023-08-22 23:33:25 +0300

    + add styles for Header component

---
+++ src/components/Header/Header.styled.ts
@@ -0,0 +1,15 @@
+import styled from '@emotion/styled';
+
+const NavList = styled.ul`
+  display: flex;
+  gap: 30px;
+  align-items: center;
+`;
+
+const HeaderWrapper = styled.div`
+  height: 115px;
+  display: flex;
+  align-items: center;
+`;
+
+export default { NavList, HeaderWrapper };
