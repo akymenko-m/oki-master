67d715236356995c69238425aa0133d20a31b7a9
Mariia Akymenko <dafnarianna@gmail.com>
2023-09-17 22:01:11 +0300

    + create about component and styles

---
+++ src/components/About/About.styled.ts
@@ -0,0 +1,144 @@
+import styled from '@emotion/styled';
+
+const Section = styled.section`
+  margin-bottom: 56px;
+
+  @media (min-width: ${(props) => props.theme.breakpoints.m}) {
+    margin-bottom: 48px;
+  }
+`;
+
+const Title = styled.h2`
+  margin-bottom: 18px;
+  font-size: ${(props) => props.theme.fontSizes.mobile.large};
+  font-weight: 500;
+  text-align: center;
+
+  @media (min-width: ${(props) => props.theme.breakpoints.m}) {
+    margin-bottom: 25px;
+    text-align: start;
+    font-size: ${(props) => props.theme.fontSizes.desktop.xxl};
+  }
+
+  @media (min-width: ${(props) => props.theme.breakpoints.l}) {
+    margin-bottom: 20px;
+    line-height: ${(props) => props.theme.lineHeight.xxl};
+  }
+`;
+
+const Content = styled.div`
+  @media (min-width: ${(props) => props.theme.breakpoints.m}) {
+    display: flex;
+  }
+
+  @media (min-width: ${(props) => props.theme.breakpoints.l}) {
+    justify-content: space-between;
+  }
+`;
+
+const ImageBlock = styled.div`
+  display: none;
+  position: relative;
+
+  @media (min-width: ${(props) => props.theme.breakpoints.m}) {
+    display: block;
+  }
+`;
+
+const Overlay = styled.div`
+  @media (min-width: ${(props) => props.theme.breakpoints.m}) {
+    top: 75px;
+    left: -67px;
+    z-index: -10;
+    width: 350px;
+    height: 430px;
+    position: absolute;
+    background-image: radial-gradient(
+      circle,
+      rgba(65, 169, 46, 0.4290966386554622) 0%,
+      rgba(188, 234, 143, 0.583158263305322) 13%,
+      rgba(255, 255, 255, 1) 65%
+    );
+    background-repeat: no-repeat;
+  }
+
+  @media (min-width: ${(props) => props.theme.breakpoints.l}) {
+    top: 63px;
+    left: -55px;
+    width: 400px;
+    height: 450px;
+    background-image: radial-gradient(
+      circle,
+      rgba(65, 169, 46, 0.4290966386554622) 0%,
+      rgba(188, 234, 143, 0.583158263305322) 13%,
+      rgba(255, 255, 255, 1) 70%
+    );
+  }
+`;
+
+const Image = styled.img`
+  object-fit: contain;
+  object-position: bottom;
+  height: 100%;
+
+  @media (min-width: ${(props) => props.theme.breakpoints.l}) {
+    object-position: top;
+  }
+`;
+
+const TextBlock = styled.div`
+  @media (min-width: ${(props) => props.theme.breakpoints.m}) {
+    max-width: 440px;
+  }
+
+  @media (min-width: ${(props) => props.theme.breakpoints.l}) {
+    max-width: 688px;
+  }
+`;
+
+const Text = styled.p`
+  margin-bottom: 16px;
+  line-height: ${(props) => props.theme.lineHeight.xs};
+
+  &:first-of-type {
+    display: none;
+  }
+
+  @media (min-width: ${(props) => props.theme.breakpoints.m}) {
+    margin-bottom: 20px;
+    font-size: ${(props) => props.theme.fontSizes.desktop.small};
+    line-height: 24px;
+
+    &:first-of-type {
+      display: block;
+      margin-bottom: 40px;
+    }
+
+    &:last-of-type {
+      display: block;
+      margin-bottom: 0;
+    }
+  }
+
+  @media (min-width: ${(props) => props.theme.breakpoints.l}) {
+    margin-bottom: 27px;
+    font-size: ${(props) => props.theme.fontSizes.desktop.medium};
+    line-height: ${(props) => props.theme.lineHeight.large};
+
+    &:first-of-type,
+    &:last-of-type {
+      margin-bottom: 0;
+    }
+  }
+`;
+
+export default {
+  Section,
+  Title,
+  Text,
+  Overlay,
+  Content,
+  TextBlock,
+  ImageBlock,
+  Image,
+};
---
+++ src/components/About/About.tsx
@@ -0,0 +1,70 @@
+import Container from '../App/App.styled';
+import styles from './About.styled';
+
+function About(): JSX.Element {
+  const {
+    Section,
+    Title,
+    Text,
+    Overlay,
+    Content,
+    TextBlock,
+    ImageBlock,
+    Image,
+  } = styles;
+
+  return (
+    <Section>
+      <Container>
+        <Content>
+          <ImageBlock>
+            <Overlay />
+
+            <picture>
+              <source
+                srcSet="./src/assets/about/іrrigator-tablet.png"
+                media="(max-width: 768px)"
+              />
+              <source
+                srcSet="./src/assets/about/іrrigator-desktop.png"
+                media="(min-width: 1280px)"
+              />
+              <Image
+                src="./src/assets/about/іrrigator-tablet.png"
+                alt="Іригатор"
+              />
+            </picture>
+          </ImageBlock>
+
+          <TextBlock>
+            <Title>
+              Про <span className="accent">нас</span>
+            </Title>
+
+            <Text>
+              Медтехніка має велике значення в повсякденному житті кожної
+              людини. Тому важливо підтримувати її в справному стані, щоб
+              будь-якої миті вона послужила вам, вірно визначила показники
+              вашого здоров’я.
+            </Text>
+            <Text>
+              <span className="accent">«ОкіМайстер»</span> з відповідальністю та
+              гарантією на роботу виконає будь-який ремонт вашого приладу,
+              надасть рекомендації з використання та догляду.
+            </Text>
+            <Text>
+              Завдяки медичній техніці люди пильнують своє добре самопочуття та
+              здоров’я, що є найголовнішим у житті, та інколи техніка ламається,
+              і не завжди є можливість придбати нову, вона може дорого
+              коштувати, і тут вам допоможе{' '}
+              <span className="accent">«ОкіМайстер»</span>, який врятує вашу
+              техніку!
+            </Text>
+          </TextBlock>
+        </Content>
+      </Container>
+    </Section>
+  );
+}
+
+export default About;
