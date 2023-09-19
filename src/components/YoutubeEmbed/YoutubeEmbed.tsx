234b3db0ee6364a8b629f8a980afa41e58c413f2
Mariia Akymenko <dafnarianna@gmail.com>
2023-09-19 22:50:50 +0300

    * change iframe for ReactPlayer component

--- src/components/YoutubeEmbed/YoutubeEmbed.tsx
+++ src/components/YoutubeEmbed/YoutubeEmbed.tsx
@@ -1,18 +1,55 @@
+import ReactPlayer from 'react-player/youtube';
+import { useRef, useState } from 'react';
+import { OnProgressProps } from 'react-player/base';
 import styles from './YoutubeEmbed.styled';
+import storage from '../../helpers/storage';
+
+const DEFAULT_STATE: OnProgressProps = {
+  playing: false,
+  seeking: false,
+  loaded: 0,
+  loadedSeconds: 0,
+  played: 0,
+  playedSeconds: 0,
+};
 
 function YoutubeEmbed(): JSX.Element {
   const { Container } = styles;
 
+  const videoPlayerRef = useRef(null);
+
+  const [videoState, setVideoState] = useState(
+    storage.load('video-progress') ?? DEFAULT_STATE
+  );
+
+  const { seeking, playing, playedSeconds } = videoState;
+
+  const progressHandler = (state: OnProgressProps) => {
+    if (!seeking && playing) {
+      setVideoState({ ...videoState, ...state });
+      storage.save('video-progress', { ...videoState, ...state });
+    }
+  };
+
+  const playPauseHandler = () => {
+    setVideoState({ ...videoState, playing: !playing });
+    storage.save('video-progress', { ...videoState, playing: !playing });
+  };
+
   return (
     <Container>
-      <iframe
-        width="560"
-        height="315"
-        src="https://www.youtube.com/embed/2EK6FGx16mo?si=FX1p_WD9YbV0YUdH"
-        title="Заміна динаміка на слуховому приладі Rexton Arena HP з використанням ущільнювального клею K704"
-        frameBorder="0"
-        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-        allowFullScreen
+      <ReactPlayer
+        config={{
+          playerVars: {
+            start: Math.round(playedSeconds),
+          },
+        }}
+        onPause={playPauseHandler}
+        onPlay={playPauseHandler}
+        onProgress={progressHandler}
+        ref={videoPlayerRef}
+        controls
+        url="https://www.youtube.com/embed/2EK6FGx16mo?si=FX1p_WD9YbV0YUdH"
       />
     </Container>
   );
