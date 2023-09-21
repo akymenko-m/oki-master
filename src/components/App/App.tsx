0d438d01d1e75b22515d440862731229d2996856
Mariia Akymenko <dafnarianna@gmail.com>
2023-09-22 00:33:30 +0300

    + add Services component

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,6 +1,7 @@
 import About from '../About/About';
 import Header from '../Header/Header';
 import Process from '../Process/Process';
+import Services from '../Services/Services';
 
 function App() {
   return (
@@ -8,6 +9,7 @@ function App() {
       <Header />
 
       <About />
+      <Services />
 
       <Process />
     </div>
