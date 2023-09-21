2782aae07190a507c3a196d877f3f6637838ba33
Mariia Akymenko <dafnarianna@gmail.com>
2023-09-22 00:32:58 +0300

    + create Services component and styles

---
+++ src/components/Services/Services.tsx
@@ -0,0 +1,26 @@
+import Container from '../App/App.styled';
+import Section from '../Section/Section';
+import Title from '../Title/Title';
+import ServicesData from '../../data/services';
+import ServicesCard from '../ServicesCard/ServicesCard';
+import styles from './Services.styled';
+
+function Services(): JSX.Element {
+  const { List } = styles;
+
+  return (
+    <Section>
+      <Container>
+        <Title text="Послуги" />
+
+        <List>
+          {ServicesData.map((el) => (
+            <ServicesCard key={el.title} data={el} />
+          ))}
+        </List>
+      </Container>
+    </Section>
+  );
+}
+
+export default Services;
