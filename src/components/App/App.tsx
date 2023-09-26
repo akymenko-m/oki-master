8b9c4414988511d6e7959b64f8ea15d375258b0f
Mariia Akymenko <dafnarianna@gmail.com>
2023-09-26 23:44:42 +0300

    + add Contacts component

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,8 +1,12 @@
 import About from '../About/About';
+import Contacts from '../Contacts/Contacts';
 import Header from '../Header/Header';
 import Process from '../Process/Process';
 import Reviews from '../Reviews/Reviews';
 import Services from '../Services/Services';
+
+const API_KEY = process.env.REACT_APP_API_KEY!;
+const MAP_URL = process.env.MAP_URL!;
 
 function App() {
   return (
@@ -15,6 +19,7 @@ function App() {
       <Process />
 
       <Reviews />
+      <Contacts apiKey={API_KEY} mapUrl={MAP_URL} />
     </div>
   );
 }
