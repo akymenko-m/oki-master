f6050eb43b7c718cf06e3d2db02db6888191c9c1
Mariia Akymenko <dafnarianna@gmail.com>
2023-09-28 23:57:05 +0300

    + add address data and interface

---
+++ src/data/address.ts
@@ -0,0 +1,11 @@
+import { IAddressData } from '../interfaces/address.interface';
+
+const AddressData: IAddressData = {
+  locationFull: 'просп. Олександра Поля, 59',
+  location: 'просп. О. Поля, 59',
+  phone: '+38(097) 7397280',
+  workDays: 'пн-пт',
+  workHours: '10:00 – 18:00',
+};
+
+export default AddressData;
