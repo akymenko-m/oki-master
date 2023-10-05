a007fc58a3f1786121dd15543b7f058fbbbe4975
Mariia Akymenko <dafnarianna@gmail.com>
2023-10-05 21:28:56 +0300

    + add OrderStatus component

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,3 +1,4 @@
+import { useEffect, useState } from 'react';
 import ScrollToHashElement from '../../helpers/scrollToHashElement';
 import About from '../About/About';
 import Contacts from '../Contacts/Contacts';
@@ -9,6 +10,7 @@ import Process from '../Process/Process';
 import Reviews from '../Reviews/Reviews';
 import Services from '../Services/Services';
 import styles from './App.styled';
+import OrderStatus from '../OrderStatus/OrderStatus';
 
 const API_KEY = process.env.REACT_APP_API_KEY!;
 const MAP_URL = process.env.MAP_URL!;
@@ -16,13 +18,28 @@ const MAP_URL = process.env.MAP_URL!;
 function App() {
   const { MainBlock } = styles;
 
+  const [showStatusOrder, setShowStatusOrder] = useState(false);
+
+  const handleStatusOrder = () => {
+    setShowStatusOrder(!showStatusOrder);
+  };
+
+  useEffect(() => {
+    if (showStatusOrder) {
+      document.body.style.overflow = 'hidden';
+    }
+    return () => {
+      document.body.style.overflow = 'unset';
+    };
+  }, [showStatusOrder]);
+
   return (
     <div>
       <ScrollToHashElement />
 
       <MainBlock>
         <Header />
-        <Hero />
+        <Hero handleStatusOrder={handleStatusOrder} />
       </MainBlock>
 
       <About />
@@ -32,6 +49,11 @@ function App() {
       <Reviews />
       <Contacts apiKey={API_KEY} mapUrl={MAP_URL} />
       <Footer />
+
+      <OrderStatus
+        open={showStatusOrder}
+        handleStatusOrder={handleStatusOrder}
+      />
     </div>
   );
 }
