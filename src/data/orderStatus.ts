3ad078020ef50d2515bb2e9e28d2a1efe8943fa6
Mariia Akymenko <dafnarianna@gmail.com>
2023-10-19 00:25:39 +0300

    + add order status data

---
+++ src/data/orderStatus.ts
@@ -0,0 +1,16 @@
+const orderStatus = [
+  {
+    status: 'accepted',
+    name: 'Прийнято',
+  },
+  {
+    status: 'atWork',
+    name: 'Ремонтується',
+  },
+  {
+    status: 'done',
+    name: 'Готово',
+  },
+];
+
+export default orderStatus;
