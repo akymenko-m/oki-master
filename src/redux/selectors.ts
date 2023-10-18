e8f59051ce2d4d5993950eda6b388ab7afc8e179
Mariia Akymenko <dafnarianna@gmail.com>
2023-10-19 00:24:54 +0300

    * add selectors sorted items, error, cur order

--- src/redux/selectors.ts
+++ src/redux/selectors.ts
@@ -1,4 +1,13 @@
+import { createSelector } from '@reduxjs/toolkit';
 import { RootState } from './store';
 
-export const getOrders = (state: RootState) => state.orders.items;
+const getOrders = (state: RootState) => state.orders.items;
+export const sortedGetOrders = createSelector(getOrders, (items) =>
+  [...items].sort(
+    (a, b) => new Date(a.date).getTime() - new Date(b.date).getTime()
+  )
+);
+
 export const getIsloading = (state: RootState) => state.orders.isLoading;
+export const getIsError = (state: RootState) => state.orders.error;
+export const getCurrentOrder = (state: RootState) => state.orders.currentOrder;
