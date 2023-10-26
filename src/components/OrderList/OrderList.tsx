3ba673a9b8b42391d362be780987c7ecaf57fa1d
Mariia Akymenko <dafnarianna@gmail.com>
2023-10-26 18:09:03 +0300

    + add props

--- src/components/OrderList/OrderList.tsx
+++ src/components/OrderList/OrderList.tsx
@@ -4,16 +4,22 @@ import styles from './OrderList.styled';
 
 interface IProps {
   orders: IItem[];
+  getOrderDetails: (arg0: IItem) => void;
+  toogleOrderDetails: () => void;
 }
 
-function OrderList({ orders }: IProps) {
-  //   console.log(orders);
+function OrderList({ orders, getOrderDetails, toogleOrderDetails }: IProps) {
   const { List } = styles;
 
   return (
     <List>
       {orders.map((order) => (
-        <OrderItem key={order.orderNumber} order={order} />
+        <OrderItem
+          key={order.orderNumber}
+          order={order}
+          getOrderDetails={getOrderDetails}
+          toogleOrderDetails={toogleOrderDetails}
+        />
       ))}
     </List>
   );
