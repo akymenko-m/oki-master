f266fc5eda2523c18fcc021e7a86810b1ad81405
Mariia Akymenko <dafnarianna@gmail.com>
2023-10-26 17:48:53 +0300

    + add operation functions

--- src/redux/ordersOperations.ts
+++ src/redux/ordersOperations.ts
@@ -3,19 +3,20 @@ import axios from 'axios';
 import { IUpdateOrderParams } from '../interfaces/admin/orderParams.interface';
 import { IItem } from '../interfaces/admin/item.interface';
 
+const BASE_URL = process.env.BASE_URL!;
+
 export const instance = axios.create({
-  baseURL: 'http://localhost:8080/api/orders',
+  baseURL: BASE_URL,
 });
 
 export const fetchOrders = createAsyncThunk(
   'orders/fetchAll',
   async (page: number, thunkApi) => {
     try {
-      const { data } = await instance.get(`/?page=${page}`);
+      const { data } = await instance.get(`/orders/?page=${page}`);
       return data;
     } catch (error) {
       const errorObject = error as Error;
-
       return thunkApi.rejectWithValue(errorObject);
     }
   }
@@ -25,11 +26,23 @@ export const fetchByQuery = createAsyncThunk(
   'orders/fetchByQuery',
   async (query: string, thunkApi) => {
     try {
-      const { data } = await instance.get(`/search?query=${query}`);
+      const { data } = await instance.get(`/orders/search?query=${query}`);
       return data;
     } catch (error) {
       const errorObject = error as Error;
+      return thunkApi.rejectWithValue(errorObject);
+    }
+  }
+);
 
+export const getOrder = createAsyncThunk(
+  'orders/getOrder',
+  async (query: string, thunkApi) => {
+    try {
+      const { data } = await instance.get(`/orders/findOne?query=${query}`);
+      return data;
+    } catch (error) {
+      const errorObject = error as Error;
       return thunkApi.rejectWithValue(errorObject);
     }
   }
@@ -39,11 +52,10 @@ export const addOrder = createAsyncThunk(
   'orders/addOrder',
   async (order: IItem, thunkApi) => {
     try {
-      const { data } = await instance.post(`/`, order);
+      const { data } = await instance.post(`/orders`, order);
       return data;
     } catch (error) {
       const errorObject = error as Error;
-
       return thunkApi.rejectWithValue(errorObject);
     }
   }
@@ -53,11 +65,10 @@ export const deleteOrder = createAsyncThunk(
   'orders/deleteOrder',
   async (orderId: string, thunkApi) => {
     try {
-      const { data } = await instance.delete(`/${orderId}`);
+      const { data } = await instance.delete(`/orders/${orderId}`);
       return data;
     } catch (error) {
       const errorObject = error as Error;
-
       return thunkApi.rejectWithValue(errorObject);
     }
   }
@@ -68,11 +79,13 @@ export const updateOrder = createAsyncThunk(
   async (obj: IUpdateOrderParams, thunkApi) => {
     const { orderId, formattedFormData } = obj;
     try {
-      const { data } = await instance.patch(`/${orderId}`, formattedFormData);
+      const { data } = await instance.patch(
+        `/orders/${orderId}`,
+        formattedFormData
+      );
       return data;
     } catch (error) {
       const errorObject = error as Error;
-
       return thunkApi.rejectWithValue(errorObject);
     }
   }
