07ec3aab4e3a584a289a52944624e0ff9b8e1bd0
Mariia Akymenko <dafnarianna@gmail.com>
2023-10-29 18:55:46 +0200

    + add reusable function inputChange for AdminPage

--- src/pages/Admin/AdminPage.styled.ts
+++ src/pages/Admin/AdminPage.styled.ts
@@ -2,15 +2,65 @@ import styled from '@emotion/styled';
 import { ReactComponent as PlusIcon } from '../../assets/plus-icon.svg';
 import { ReactComponent as ArchiveIcon } from '../../assets/archive-icon.svg';
 import { ReactComponent as BackIcon } from '../../assets/back-icon.svg';
+import { ReactComponent as OrdersIcon } from '../../assets/orders-icon.svg';
+
+const Overlay = styled.div`
+  @media (min-width: ${(props) => props.theme.breakpoints.m}) {
+    position: fixed;
+    top: 0;
+    left: 0;
+    z-index: 2;
+    width: 100vw;
+    height: 100vh;
+    background: rgba(255, 255, 255, 0.5);
+    overflow-y: scroll;
+  }
+`;
+
+const Modal = styled.div`
+  width: 100%;
+  height: 100%;
+  position: fixed;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  overflow-y: auto;
+  z-index: 1;
+  background-color: ${(props) => props.theme.colors.white};
+
+  @media (min-width: ${(props) => props.theme.breakpoints.m}) {
+    height: auto;
+    max-width: 600px;
+    position: relative;
+    top: 20px;
+    transform: translate(-50%, 0);
+    border: 2px solid ${(props) => props.theme.colors.dark};
+    border-radius: 24px;
+  }
+
+  @media (min-width: ${(props) => props.theme.breakpoints.l}) {
+    max-width: 886px;
+  }
+`;
 
 const MainBlock = styled.div`
   padding: 24px 0;
   position: relative;
 
   @media (min-width: ${(props) => props.theme.breakpoints.m}) {
+    padding: 40px 0;
+
+    &.order {
+      padding: 24px 0;
+    }
   }
 
   @media (min-width: ${(props) => props.theme.breakpoints.l}) {
+    padding: 48px 0;
+
+    &.order {
+      padding: 0;
+    }
   }
 `;
 
@@ -20,9 +70,31 @@ const HeaderBlock = styled.div`
   position: relative;
 
   @media (min-width: ${(props) => props.theme.breakpoints.m}) {
+    &.order {
+      margin-bottom: 0;
+      padding-bottom: 36px;
+      border-bottom: 2px solid ${(props) => props.theme.colors.dark};
+    }
+
+    &.admin-page {
+      display: none;
+    }
   }
 
   @media (min-width: ${(props) => props.theme.breakpoints.l}) {
+    padding: 40px 43px;
+  }
+`;
+
+const HeaderAdmin = styled.div`
+  display: none;
+
+  @media (min-width: ${(props) => props.theme.breakpoints.m}) {
+    padding: 10px 0;
+    display: flex;
+    align-items: center;
+    gap: 16px;
+    border-bottom: 2px solid ${(props) => props.theme.colors.dark};
   }
 `;
 
@@ -34,21 +106,39 @@ const Title = styled.h2`
   text-align: center;
 
   @media (min-width: ${(props) => props.theme.breakpoints.m}) {
+    min-width: max-content;
+    font-size: ${(props) => props.theme.fontSizes.desktop.xs};
+    line-height: ${(props) => props.theme.lineHeight.small};
+    line-height: 30px;
+
+    &.order {
+      font-size: ${(props) => props.theme.fontSizes.desktop.small};
+    }
   }
 
   @media (min-width: ${(props) => props.theme.breakpoints.l}) {
+    &.order {
+      font-size: ${(props) => props.theme.fontSizes.desktop.medium};
+    }
   }
 `;
 
-const StyledCloseIcon = styled(PlusIcon)`
+const StyledPlusIcon = styled(PlusIcon)`
   width: 38px;
   height: 38px;
   fill: ${(props) => props.theme.colors.dark};
+  transition: fill 250ms ${(props) => props.theme.animations.cubicBezier};
 
-  @media (min-width: ${(props) => props.theme.breakpoints.m}) {
+  &.header {
+    width: 25px;
+    height: 25px;
   }
 
   @media (min-width: ${(props) => props.theme.breakpoints.l}) {
+    &.header {
+      width: 30px;
+      height: 30px;
+    }
   }
 `;
 
@@ -57,10 +147,17 @@ const StyledArchiveIcon = styled(ArchiveIcon)`
   height: 25px;
   stroke: ${(props) => props.theme.colors.accent};
 
-  @media (min-width: ${(props) => props.theme.breakpoints.m}) {
+  &.header {
+    width: 25px;
+    height: 25px;
+    stroke: ${(props) => props.theme.colors.dark};
   }
 
   @media (min-width: ${(props) => props.theme.breakpoints.l}) {
+    &.header {
+      width: 30px;
+      height: 30px;
+    }
   }
 `;
 
@@ -68,43 +165,23 @@ const StyledBackIcon = styled(BackIcon)`
   width: 25px;
   height: 25px;
   stroke: ${(props) => props.theme.colors.accent};
-
-  @media (min-width: ${(props) => props.theme.breakpoints.m}) {
-  }
-
-  @media (min-width: ${(props) => props.theme.breakpoints.l}) {
-  }
 `;
 
-const Modal = styled.div`
-  width: 100%;
-  height: 100%;
-  position: absolute;
-  top: 0;
-  left: 0;
-  z-index: 1;
-  background-color: ${(props) => props.theme.colors.white};
-  /* margin: 0 auto; */
-  /* margin-top: 50px; */
-  /* padding: 20px 11px; */
-  /* display: flex;
-  flex-direction: column;
-  gap: 10px;
-  align-items: center; */
-
-  @media (min-width: ${(props) => props.theme.breakpoints.m}) {
-  }
-
-  @media (min-width: ${(props) => props.theme.breakpoints.l}) {
-  }
+const StyledOrdersIcon = styled(OrdersIcon)`
+  width: 30px;
+  height: 30px;
+  fill: ${(props) => props.theme.colors.dark};
 `;
 
 export default {
   MainBlock,
   HeaderBlock,
   Title,
-  StyledCloseIcon,
+  StyledPlusIcon,
   Modal,
   StyledArchiveIcon,
   StyledBackIcon,
+  Overlay,
+  HeaderAdmin,
+  StyledOrdersIcon,
 };
