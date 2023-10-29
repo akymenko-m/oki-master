a66fd6887a96f269ed64e51eb98ad14d83cbba55
Mariia Akymenko <dafnarianna@gmail.com>
2023-10-29 18:54:14 +0200

    + add overlay and props

--- src/components/OrderLayout/OrderLayout.tsx
+++ src/components/OrderLayout/OrderLayout.tsx
@@ -1,7 +1,6 @@
 import Logo from '../Logo/Logo';
 import { ReactComponent as CloseIcon } from '../../assets/close-icon.svg';
 import Button from '../Button/Button';
-import appStyles from '../App/App.styled';
 import adminPageStyles from '../../pages/Admin/AdminPage.styled';
 
 interface IProps extends React.ComponentPropsWithoutRef<'div'> {
@@ -9,31 +8,31 @@ interface IProps extends React.ComponentPropsWithoutRef<'div'> {
   title: string;
 }
 
-function OrderLayout({ toggleModal, title, children }: IProps): JSX.Element {
-  const { Container } = appStyles;
-  const { Modal, Title, HeaderBlock, MainBlock } = adminPageStyles;
+function OrderLayout({
+  toggleModal,
+  title,
+  children,
+  className,
+}: IProps): JSX.Element {
+  const { Modal, Title, HeaderBlock, MainBlock, Overlay } = adminPageStyles;
 
   return (
-    <Modal>
-      <Container>
-        <MainBlock>
-          <HeaderBlock>
-            <Logo className="admin" />
-            <Title>{title}</Title>
+    <Overlay>
+      <Modal>
+        <MainBlock className="order">
+          <HeaderBlock className={className}>
+            <Logo className={`${className} admin`} />
+            <Title className="order">{title}</Title>
 
-            <Button
-              type="button"
-              className="order-status order"
-              onClick={toggleModal}
-            >
+            <Button type="button" className="order" onClick={toggleModal}>
               <CloseIcon />
             </Button>
           </HeaderBlock>
 
           {children}
         </MainBlock>
-      </Container>
-    </Modal>
+      </Modal>
+    </Overlay>
   );
 }
 
